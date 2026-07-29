a93c8b8631601ae8478edf5ce0c2ddd5bb9916e0
Bloberose <bloberose06@gmail.com>
2026-07-29 17:07:51 -0600

    Remove Header and Footer from Home component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,3 @@
-
 import Hero from "@/app/components/Hero";
 import Tours from "@/app/components/Tours";
 import Gallery from "@/app/components/Gallery";
@@ -6,20 +5,15 @@ import WhyUs from "@/app/components/WhyUs";
 import Calendar from "@/app/components/Calendar";
 import Reservation from "@/app/components/Reservation";
 
-
 export default function Home() {
   return (
     <>
-      <Header />
-      <main className="flex-1">
-        <Hero />
-        <Tours />
-        <Gallery />
-        <WhyUs />
-        <Calendar />
-        <Reservation />
-      </main>
-      <Footer />
+      <Hero />
+      <Tours />
+      <Gallery />
+      <WhyUs />
+      <Calendar />
+      <Reservation />
     </>
   );
 }
