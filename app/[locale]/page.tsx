425d7b5d18d58f9218a200f2a54320258697b727
Bloberose <bloberose06@gmail.com>
2026-07-29 16:57:03 -0600

    Remove unused Header and Footer imports
    Removed Header and Footer imports from page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,11 @@
-import Header from "@/app/components/Header";
+
 import Hero from "@/app/components/Hero";
 import Tours from "@/app/components/Tours";
 import Gallery from "@/app/components/Gallery";
 import WhyUs from "@/app/components/WhyUs";
 import Calendar from "@/app/components/Calendar";
 import Reservation from "@/app/components/Reservation";
-import Footer from "@/app/components/Footer";
+
 
 export default function Home() {
   return (
