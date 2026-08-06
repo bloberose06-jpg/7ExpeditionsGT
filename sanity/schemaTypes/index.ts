92fc76561c7006d89544bf1c02375c2828fa5a46
Bloberose <bloberose06@gmail.com>
2026-08-06 09:12:43 -0600

    Add volcano schema to schemaTypes and schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,8 +1,9 @@
 import { tourType } from './tour'
 import vlog from './vlog'
+import { volcano } from './volcano' // 1. Importas el nuevo schema
 
-export const schemaTypes = [tourType, vlog]
+export const schemaTypes = [tourType, vlog, volcano] // 2. Lo agregas aquí
 
 export const schema = {
-  types: [tourType, vlog],
+  types: [tourType, vlog, volcano], // 3. Y lo agregas aquí también
 }
