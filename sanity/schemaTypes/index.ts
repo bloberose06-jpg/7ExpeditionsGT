3dbc0ac0ecdb4f8fe4953a657b5ab75d3b5165f8
Bloberose <bloberose06@gmail.com>
2026-08-20 14:48:20 -0600

    Add destination to schemaTypes and schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,9 +2,10 @@ import { tourType } from './tour'
 import vlog from './vlog'
 import { volcano } from './volcano'
 import instagramPost from './instagramPost'
+import { destination } from './destination' // 1. Import destination
 
-export const schemaTypes = [tourType, vlog, volcano, instagramPost]
+export const schemaTypes = [tourType, vlog, volcano, instagramPost, destination] // 2. Add here
 
 export const schema = {
-  types: [tourType, vlog, volcano, instagramPost],
+  types: [tourType, vlog, volcano, instagramPost, destination], // 3. Add here
 }
