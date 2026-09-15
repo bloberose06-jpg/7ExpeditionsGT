7204a064a8f70cd10afb2cacfb68df97f2618c18
Bloberose <bloberose06@gmail.com>
2026-09-15 17:53:40 -0600

    Comment out InstagramScroll import
    Commented out the InstagramScroll import statement.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import Hero from "@/app/components/Hero";
 import Tours from "@/app/components/Tours";
 import Gallery from "@/app/components/Gallery";
-import InstagramScroll, { mockInstagramPosts } from "@/app/components/InstagramScroll";
+//import InstagramScroll, { mockInstagramPosts } from "@/app/components/InstagramScroll";
 import WhyUs from "@/app/components/WhyUs";
 //import Calendar from "@/app/components/Calendar";
 import Reservation from "@/app/components/Reservation";
