8b857c8a335ec7a624410dd577a2a923a051d31e
Bloberose <bloberose06@gmail.com>
2026-09-15 18:00:33 -0600

    Rename TripAdvisorWidget to Reviews and update layout

--- app/components/TripAdvisorWidget.tsx
+++ app/components/TripAdvisorWidget.tsx
@@ -1,34 +1,21 @@
-
 'use client';
 
 import Script from 'next/script';
 
-export default function TripAdvisorWidget() {
+export default function Reviews() {
   return (
-    <div className="flex justify-center my-6 min-h-[120px]">
-      {/* Contenedor oficial de TripAdvisor */}
-      <div id="TA_selfserveprop301" className="TA_selfserveprop">
-        <ul id="jbR81Uey" className="TA_links tq7GZOuIFrU">
-          <li id="oFBmme" className="LjXRSXgsRG">
-            <a 
-              target="_blank" 
-              rel="noopener noreferrer" 
-              href="https://www.tripadvisor.com/Attraction_Review-g295366-d34516424-Reviews-7_Expeditions_Guatemala-Antigua_Sacatepequez_Department.html"
-            >
-              <img 
-                src="https://www.tripadvisor.com/img/cdsi/img2/branding/v2/Tripadvisor_lockup_horizontal_secondary_registered-11900-2.svg" 
-                alt="TripAdvisor"
-              />
-            </a>
-          </li>
-        </ul>
-      </div>
+    <section className="py-12 bg-zinc-950 text-white min-h-[250px]">
+      <div className="container mx-auto px-4 max-w-6xl">
+        <h2 className="text-2xl font-bold text-center mb-8 uppercase tracking-wide">
+          Opiniones de nuestros viajeros
+        </h2>
 
-      {/* Script optimizado para React/Next.js */}
-      <Script 
-        src="https://www.jscache.com/wejs?wtype=selfserveprop&uniq=301&locationId=34516424&lang=en_US&rating=true&nreviews=5&writereviewlink=true&popIdx=true&iswide=false&border=true&display_version=2" 
-        strategy="lazyOnload" 
-      />
-    </div>
+        {/* Carga del script oficial de Trustindex */}
+        <Script 
+          src="https://cdn.trustindex.io/loader.js?2e3d5b781f6a550d7986e17d845" 
+          strategy="lazyOnload" 
+        />
+      </div>
+    </section>
   );
 }
