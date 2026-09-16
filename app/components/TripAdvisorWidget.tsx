e1bfd319950fbbab0b3ef50fd96c26b86335fb80
Bloberose <bloberose06@gmail.com>
2026-09-15 18:03:19 -0600

    Rename Reviews component to TripAdvisorWidget

--- app/components/TripAdvisorWidget.tsx
+++ app/components/TripAdvisorWidget.tsx
@@ -2,7 +2,7 @@
 
 import Script from 'next/script';
 
-export default function Reviews() {
+export default function TripAdvisorWidget() {
   return (
     <section className="py-12 bg-zinc-950 text-white min-h-[250px]">
       <div className="container mx-auto px-4 max-w-6xl">
@@ -10,7 +10,10 @@ export default function Reviews() {
           Opiniones de nuestros viajeros
         </h2>
 
-        {/* Carga del script oficial de Trustindex */}
+        {/* Contenedor target obligatorio para Trustindex */}
+        <div id="trustindex-widget"></div>
+
+        {/* Script de Trustindex */}
         <Script 
           src="https://cdn.trustindex.io/loader.js?2e3d5b781f6a550d7986e17d845" 
           strategy="lazyOnload" 
