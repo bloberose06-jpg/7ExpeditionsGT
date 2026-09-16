26be668ec741ec58bd0ad8b043d2dfb63bf275aa
Bloberose <bloberose06@gmail.com>
2026-09-16 17:24:59 -0600

    Refactor TripAdvisorWidget component structure

--- app/components/TripAdvisorWidget.tsx
+++ app/components/TripAdvisorWidget.tsx
@@ -6,15 +6,13 @@ export default function TripAdvisorWidget() {
   return (
     <section className="py-12 bg-black text-white border-t border-zinc-900">
       <div className="container mx-auto px-4 max-w-6xl">
-        {/* Contenedor oficial de SociableKIT */}
         <div 
-          className="sk-ww-tripadvisor-reviews min-h-[200px]" 
+          className="sk-ww-tripadvisor-reviews" 
           data-embed-id="25714443"
         ></div>
 
-        {/* Script de SociableKIT */}
-        <Script
-          src="https://widgets.sociablekit.com/tripadvisor-reviews/widget.js"
+        <Script 
+          src="https://widgets.sociablekit.com/tripadvisor-reviews/widget.js" 
           strategy="lazyOnload"
         />
       </div>
